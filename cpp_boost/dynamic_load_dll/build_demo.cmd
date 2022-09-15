0527c6d73132afb628c73926566b757022d0fab5
davied <wuwei_543333827@126.com>
2022-09-15 22:58:41 +0800

    code cleanup

--- cpp_boost/dynamic_load_dll/build_demo.cmd
+++ cpp_boost/dynamic_load_dll/build_demo.cmd
@@ -6,7 +6,7 @@ set BUILD_DIR=%SOURCE_DIR%.build
 rmdir /s /q %BUILD_DIR%
 
 mkdir %BUILD_DIR%
-cmake -S %SOURCE_DIR% -B %BUILD_DIR% -DBoost_DEBUG=1
+cmake -S %SOURCE_DIR% -B %BUILD_DIR%
 cmake --build %BUILD_DIR% --config Debug
 %BUILD_DIR%\Debug\lib_ref_demo.exe
 cmake --build %BUILD_DIR% --config Release
