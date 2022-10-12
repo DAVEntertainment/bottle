ece6a90aaac87207f0ea324bfcb1a635c12588dd
davied <wuwei_543333827@126.com>
2022-10-12 22:31:47 +0800

    support local source files for conan create

--- cpp_with_conan/create_shared_library_with_conan/conan_recipe/all/conanfile.py
+++ cpp_with_conan/create_shared_library_with_conan/conan_recipe/all/conanfile.py
@@ -1,6 +1,12 @@
 from conans import ConanFile, CMake, tools
+from conan.tools.files import update_conandata
 from functools import lru_cache
 from os.path import join as joinpath
+from os.path import split as splitpath
+from os.path import basename, dirname, abspath
+from os import getcwd, environ
+from sys import argv
+from shutil import copy, rmtree, copytree
 
 
 class HelloConan(ConanFile):
@@ -12,31 +18,46 @@ class HelloConan(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain"
     settings = "os", "arch", "compiler", "build_type"
 
+    def export(self):
+        source_dir = joinpath(dirname(__file__), '..', self.name)
+
+        update_conandata(self, {
+            "source_origin": abspath(source_dir),
+            "__file__ in export": __file__
+        })
+
     def source(self):
+        source_origin = self.conan_data["source_origin"]
+        source_dir = abspath(joinpath(getcwd(), basename(source_origin)))
+
         print("@@@ start source phase")
+        print("@@@ working in: {}".format(getcwd()))
         print("@@@ conan_data: {}".format(self.conan_data))
         print("@@@ version: {}".format(self.version))
-        # self.conan_data, loaded from conandata.yml
-        tools.get(**self.conan_data["sources"][self.version], strip_root = True)
+        print("@@@ source_origin: {}".format(source_origin))
+        print("@@@ source_dir: {}".format(source_dir))
+
+        copytree(source_origin, source_dir)
 
     @lru_cache(1)
     def _configure(self):
         print("@@@ configuring cmake")
+        print("@@@ working in: {}".format(getcwd()))
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure(source_folder=self.name) # we use copytree copy src files into a folder, so we need to specify source folder here
         return cmake
 
     def build(self):
         cmake = self._configure()
         print("@@@ start build")
+        print("@@@ working in: {}".format(getcwd()))
         cmake.build()
 
     def package(self):
         cmake = self._configure()
         print("@@@ start install")
+        print("@@@ working in: {}".format(getcwd()))
         cmake.install()
-        tools.rmdir(joinpath(self.package_folder, "lib", "cmake"))
-        tools.remove_files_by_mask(joinpath(self.package_folder, "lib"), "*.pdb")
 
     def package_info(self):
         self.cpp_info.libs = ["add"]
